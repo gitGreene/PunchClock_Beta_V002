f6b3e3cacbfa766f8bac9f3081eeddb46087ab68
Ian <m.iangreene@gmail.com>
2018-10-08 15:04:53 -0400

    Day 020 of 100DoCC

---
+++ app/src/main/java/co/codemaestro/punchclockv002/CommitTimeFragment.java
@@ -0,0 +1,35 @@
+package co.codemaestro.punchclockv002;
+
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CommitTimeFragment extends DialogFragment implements android.view.View.OnClickListener {
+
+
+    public CommitTimeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
