6b18538c88d6bebc7a4c9cb5990682a1a6c097ce
Francis <francisglacken@gmail.com>
2018-10-09 16:59:24 -0400

    Added small code to MainActivity, added RecyclerView functionality with "TimeDatabaseAdapter.java" and "timeDatabase.java", deleted "activity_frame_layout"
    10/9/2018

--- app/src/main/java/co/codemaestro/punchclockv002/timeDatabase.java
+++ app/src/main/java/co/codemaestro/punchclockv002/timeDatabase.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+import java.util.LinkedList;
 
+// Refactor the name to "TimeDataBase" with the right caps
 public class timeDatabase extends AppCompatActivity {
 
-    Long millisecondsTime;
-    String category, timeBankName;
+    String category, timeBankName, savedTime;
+    private final LinkedList<String> myDataSet = new LinkedList<>();
+    private RecyclerView myRecyclerView;
+    private TimeDatabaseAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +23,47 @@ public class timeDatabase extends AppCompatActivity {
         setContentView(R.layout.activity_time_database);
 
         Intent turnMeOn = getIntent();
-        timeBankName = turnMeOn.getStringExtra("TIME_BANK_NAME");
-        category = turnMeOn.getStringExtra("CATEGORY_NAME");
-        millisecondsTime = turnMeOn.getLongExtra("CURRENT_TIME", 0L);
+        //timeBankName = turnMeOn.getStringExtra("TIME_BANK_NAME");
+        //category = turnMeOn.getStringExtra("CATEGORY_NAME");
+        savedTime = turnMeOn.getStringExtra("CURRENT_TIME");
+
+        // Put data in the list "Dataset" as a String
+        myDataSet.addLast(savedTime);
+
+        myRecyclerView = findViewById(R.id.timeRecyclerView);
+
+        //Create an adapter then supply the data
+        myAdapter = new TimeDatabaseAdapter(this, myDataSet);
+
+        // Connects the adapter to the RecView
+        myRecyclerView.setAdapter(myAdapter);
+
+        // Gives the RecView a default Layout Manager
+        myRecyclerView.setLayoutManager(new LinearLayoutManager((this)));
+
+
+
+
 
     }
+
+    public void tempAddToList(View view) {
+
+        // Put data in the list "Dataset" as a String
+        myDataSet.addLast(savedTime);
+
+        myRecyclerView = findViewById(R.id.timeRecyclerView);
+
+        //Create an adapter then supply the data
+        myAdapter = new TimeDatabaseAdapter(this, myDataSet);
+
+        // Connects the adapter to the RecView
+        myRecyclerView.setAdapter(myAdapter);
+
+        // Gives the RecView a default Layout Manager
+        myRecyclerView.setLayoutManager(new LinearLayoutManager((this)));
+
+    }
+
+
 }
