0ad607cc083d4a05ccd583d27690f09f446946fd
Francis <francisglacken@gmail.com>
2018-11-01 23:30:35 -0400

    test comment 2

--- app/src/main/java/co/codemaestro/punchclockv002/MainActivity.java
+++ app/src/main/java/co/codemaestro/punchclockv002/MainActivity.java
@@ -15,7 +15,7 @@ import android.os.Handler;
 public class MainActivity extends AppCompatActivity
                         implements CommitTimeDialog.CommitTimeFragmentListener {
 
-    //TESTs are for bitches//
+    //TESTs are for bitches and hoes//
     TextView timeView;
     Button start, pause, reset;
     Long millisecondsTime, StartTime, TimeBuff = 0L, UpdateTime = 0L;
